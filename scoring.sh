16cc12a803bad049fdfa1f8b6b930492c5a9d608
Cyber-30 <souryadutta234@gmail.com>
2026-03-03 01:50:06 +0530

    Issue fix

--- scoring.sh
+++ scoring.sh
@@ -10,6 +10,7 @@
 
 TOTAL_SCORE=0
 TOTAL_MAX=0
+PERCENTAGE=0
 
 USER_SCORE=0
 FILESYSTEM_SCORE=0
@@ -32,17 +33,20 @@ RECOMMENDATIONS=()
 init_score() {
     TOTAL_SCORE=0
     TOTAL_MAX=0
+    PERCENTAGE=0
+
     USER_SCORE=0
     FILESYSTEM_SCORE=0
     NETWORK_SCORE=0
     SERVICE_SCORE=0
     HARDENING_SCORE=0
+
     COMPROMISED=0
     RECOMMENDATIONS=()
 }
 
 ########################################
-# Add Score To Category
+# Add Score To Category (CAPPED)
 ########################################
 add_score() {
     local category="$1"
@@ -51,22 +55,25 @@ add_score() {
     case "$category" in
         USER)
             USER_SCORE=$((USER_SCORE + points))
+            [[ $USER_SCORE -gt $USER_MAX ]] && USER_SCORE=$USER_MAX
             ;;
         FILESYSTEM)
             FILESYSTEM_SCORE=$((FILESYSTEM_SCORE + points))
+            [[ $FILESYSTEM_SCORE -gt $FILESYSTEM_MAX ]] && FILESYSTEM_SCORE=$FILESYSTEM_MAX
             ;;
         NETWORK)
             NETWORK_SCORE=$((NETWORK_SCORE + points))
+            [[ $NETWORK_SCORE -gt $NETWORK_MAX ]] && NETWORK_SCORE=$NETWORK_MAX
             ;;
         SERVICE)
             SERVICE_SCORE=$((SERVICE_SCORE + points))
+            [[ $SERVICE_SCORE -gt $SERVICE_MAX ]] && SERVICE_SCORE=$SERVICE_MAX
             ;;
         HARDENING)
             HARDENING_SCORE=$((HARDENING_SCORE + points))
+            [[ $HARDENING_SCORE -gt $HARDENING_MAX ]] && HARDENING_SCORE=$HARDENING_MAX
             ;;
     esac
-
-    TOTAL_SCORE=$((TOTAL_SCORE + points))
 }
 
 ########################################
@@ -84,12 +91,17 @@ mark_compromised() {
 }
 
 ########################################
-# Calculate Final Risk
+# Calculate Final Risk (CORRECTED)
 ########################################
 calculate_risk() {
 
     TOTAL_MAX=$((USER_MAX + FILESYSTEM_MAX + NETWORK_MAX + SERVICE_MAX + HARDENING_MAX))
-    PERCENTAGE=$(( TOTAL_SCORE * 100 / TOTAL_MAX ))
+
+    # Recalculate total from capped section values
+    TOTAL_SCORE=$((USER_SCORE + FILESYSTEM_SCORE + NETWORK_SCORE + SERVICE_SCORE + HARDENING_SCORE))
+
+    # Proper rounded percentage
+    PERCENTAGE=$(awk "BEGIN {printf \"%d\", ($TOTAL_SCORE/$TOTAL_MAX)*100}")
 
     if [[ $COMPROMISED -eq 1 ]]; then
         RISK_LEVEL="COMPROMISED"
@@ -127,7 +139,7 @@ print_final_score() {
 
     echo "-----------------------------------------"
     printf "FINAL SECURITY SCORE: %d / %d (%d%%)\n" "$TOTAL_SCORE" "$TOTAL_MAX" "$PERCENTAGE"
-    echo "RISK LEVEL          : $RISK_LEVEL"
+    echo "RISK LEVEL          : %s\n" "$RISK_LEVEL"
     echo "========================================="
 
     echo
